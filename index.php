af803f1275d8f7d038d0198b34473a9cbf8fe133
Felipe <felipe-almeida2012@hotmail.com>
2019-03-26 19:34:40 -0300

    Metódos de lista.

--- index.php
+++ index.php
@@ -2,16 +2,31 @@
 
 require_once("config.php");
 
+
+// Carrega um usuário.
+/*
 $root = new Usuario();
 
 $root->loadById(3);
 
-echo $root;
+echo $root;*/
 
-/*$sql = new Sql();
+//Carrega uma lista de usuários.
+/*
+$lista = Usuario::getList();
 
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+echo json_encode($lista);
+*/
 
-echo json_encode($usuarios);*/
+// Carrega uma lista de usuários buscando pelo login.
+/*$search = Usuario::search("Fe");
+
+echo json_encode($search);*/
+
+// Carrega um usuário usando o login e a senha.
+$usuario = new Usuario();
+$usuario->login("Felipe", "124567890");
+
+echo $usuario;
 
  ?>
